6706a34e86299526c1c82c2a42febe24d349d266
sunhee99 <sunhily99@gmail.com>
2021-02-05 02:42:58 +0900

    #3 STATE

--- src/App.js
+++ src/App.js
@@ -2,34 +2,18 @@ import React from "react";
 
 class App extends React.Component {
   state = {
-    count: 0,
-  }; //변경할 데이터를 state에 넣는다.
-
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
+    isLoading: true,
+    movies: [],
   };
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
-  }; //변경할 데이터를 state에서 직접적으로 변경하지 말아야한다.
-
   componentDidMount() {
-    console.log("component rendered");
-  }
-  componentDidUpdate() {
-    console.log("I just update");
-  }
-  componentWillMount() {
-    console.log("Goodbye, cruel world");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+    // 시간차를 주어 변경되게한다.
   }
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
